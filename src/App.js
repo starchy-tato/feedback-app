4782e4f66ad03420ccff768d8b23a22a823ce0b2
Tiffanie Wong <tlwong1993@hotmail.com>
2022-08-22 22:10:56 -0600

    create header
    * add default prop
    * add prop type

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Header from "./components/Header"
+
 function App(){
   return (
-    <div className='container'>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Header text={true} />
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
+
   )
 }
 
