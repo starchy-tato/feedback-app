be78ac9d5237ec7c3fdd2e2d3041d1ee4ca592b0
Tiffanie Wong <tlwong1993@hotmail.com>
2022-08-26 00:32:12 -0600

    add styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header"
 function App(){
   return (
     <>
-      <Header text={true} />
+      <Header />
       <div className='container'>
         <h1>My App</h1>
       </div>
@@ -11,5 +11,4 @@ function App(){
 
   )
 }
-
 export default App
