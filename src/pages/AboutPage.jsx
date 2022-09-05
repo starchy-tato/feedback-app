0d1305e4f24ca392b8d628b417162c8dfe1647f4
Tiffanie Wong <tlwong1993@hotmail.com>
2022-09-05 17:09:01 -0600

    add about page
    * installed react router 6
    * update app with routes
    * create new about page component

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+import Card from "../components/shared/Card"
+
+function AboutPage() {
+  return(
+    <div>
+      <div className='about'>
+        <h1>About This Project</h1>
+        <p>This is a React app to leave feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+
+        <p href='/'>Back to Home</p>
+      </div>
+    </div>
+  )
+}
+
+export default AboutPage
