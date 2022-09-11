e664f51881475fcded6834bf55e652162aaa2208
Tiffanie Wong <tlwong1993@hotmail.com>
2022-09-10 23:05:45 -0600

    update dummy data

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -12,12 +12,12 @@ export const FeedbackProvider = ({children}) => {
     },
     {
       id: 2,
-      text: 'This is feedback item 1',
+      text: 'This is feedback item 2',
       rating: 9
     },
     {
       id: 3,
-      text: 'This is feedback item 1',
+      text: 'This is feedback item 3',
       rating: 7
     }
   ])
